f61466d7fedfd48ce04051437ec51a92eb7fd3b0
Mahadevan1234 <mahi17700@gmail.com>
2024-02-10 16:12:52 +0530

    admin page

---
+++ controllers/adminRouter.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const bcrypt=require("bcryptjs")
+const { genSalt } = require("bcrypt")
+const studentModel = require("../models/studentModel")
+
+const router=express.Router()
+
+hashedPassword=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
+router.post("/addstudent",async(req,res)=>{
+    let {data}={"data":req.body}
+    let password=data.password
+    hashedPassword(password).then(
+        (hashpass)=>{
+            console.log(hashpass)
+            data.password=hashpass
+            console.log(data)
+            let student=new studentModel(data)
+            let result=student.save()
+            res.json({
+                status:"success"
+            })
+        }
+    )
+})
+
+module.exports=router
